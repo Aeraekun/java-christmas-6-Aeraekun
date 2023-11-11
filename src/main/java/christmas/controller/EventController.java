7375bbcff310e00a3331a6f33e27951d02b798bf
Aeraekun <lsch3374@naver.com>
2023-11-11 22:44:30 +0900

    feat: EventController ################ 함수 목업 작성 ################
    ################
    # feat : 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # cicd : CI/CD 관련 설정
    ################

---
+++ src/main/java/christmas/controller/EventController.java
@@ -0,0 +1,48 @@
+package christmas.controller;
+
+import static christmas.constant.message.OutputMessage.*;
+public class EventController {
+    public void event() {
+
+    }
+
+    public void printStart() {
+
+    }
+
+    public void getDate() {
+
+    }
+
+    public void getMenu() {
+
+    }
+
+    public void printMenu() {
+
+    }
+
+    public void printBeforeDiscount() {
+
+    }
+
+    public void applyEvent() {
+
+    }
+
+    public void printGiftMenu() {
+
+    }
+
+    public void printDiscount() {
+
+    }
+
+    public void printAfterDiscount() {
+
+    }
+
+    public void printEventBadge() {
+
+    }
+}
