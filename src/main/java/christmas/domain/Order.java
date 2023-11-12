eb0bb10906857e77bd3ff897f0cac499cf34406b
Aeraekun <lsch3374@naver.com>
2023-11-13 03:22:22 +0900

    feat: Order ################ Test코드 예외 처리 추가 ################
    ################
    # feat : 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refactor : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    # cicd : CI/CD 관련 설정
    ################

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,20 +1,41 @@
 package christmas.domain;
 
+import christmas.domain.menu.Drink;
 import christmas.domain.menu.Menu;
 
 import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-import static christmas.constant.Number.MAX_DATE;
-import static christmas.constant.Number.MIN_DATE;
 import static christmas.constant.message.ErrorMessage.INVALID_ORDER;
 
 public record Order(EnumMap<Menu, Integer> order) {
 
-    public Order{
+    public Order {
+        validate(order);
     }
 
-    private void validate(int date) {
-        if (date < MIN_DATE || date > MAX_DATE) {
+    private void validate(EnumMap<Menu, Integer> order) {
+        Set<String> names = new HashSet<>();
+        for (Map.Entry<Menu, Integer> entry : order.entrySet()) {
+            Menu menu = entry.getKey();
+            Integer i = entry.getValue();
+            names.add(menu.name());
+            if (i <= 0) {
+                throw new IllegalArgumentException(INVALID_ORDER);
+            }
+        }
+        onlyDrink(names);
+    }
+
+    private void onlyDrink(Set<String> names) {
+        for (String s : names) {
+            try {
+                Drink.valueOf(s);
+            } catch (IllegalArgumentException e) {
+                break;
+            }
             throw new IllegalArgumentException(INVALID_ORDER);
         }
     }
--- src/main/java/christmas/view/InputOrderView.java
+++ src/main/java/christmas/view/InputOrderView.java
@@ -3,8 +3,11 @@ package christmas.view;
 import christmas.domain.menu.Menu;
 
 import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static christmas.constant.message.ErrorMessage.INVALID_ORDER;
 import static christmas.constant.message.InputMessage.ORDER;
 
 public class InputOrderView {
@@ -18,11 +21,15 @@ public class InputOrderView {
 
     private static void typeCasting() {
         String[] items = readLine().split(",");
+        Set<String> names = new HashSet<>();
         for (String item : items) {
             String[] parts = item.split("-");
             Menu menuItem = Menu.valueOf(parts[0]);
             int quantity = Integer.parseInt(parts[1]);
-            System.out.println(menuItem + parts[1]);
+            if (names.contains(parts[0])) {
+                throw new IllegalArgumentException(INVALID_ORDER);
+            }
+            names.add(parts[0]);
             order.put(menuItem, quantity);
         }
     }
